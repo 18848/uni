4e4f88ba3b03a00565a6a7b1c6e8c29fae588947
Leonel Fernandes <58857998+GestindioAlfoice@users.noreply.github.com>
2021-11-16 12:08:24 +0000

    segundo

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ s = open("schedule.json", "r")
 schedule = json.load(s)
 s.close()
 
+f = open("funcs.json", "r")
+funcs = json.load(f)
+f.close()
 
+p = open("previsoes.json", "r")
+predictions = json.load(p)
+p.close()
+
+
+print(schedule)
+print(funcs)
+print(predictions)
 
